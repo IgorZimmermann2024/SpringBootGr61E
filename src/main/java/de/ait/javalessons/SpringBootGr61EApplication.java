28942d943ad80ae3f34a4e39774c9a0bcfad3d4f
Igor Zimmermann <igor.sonstnix@gmail.com>
2025-06-10 20:42:50 +0200

    initial commit

---
+++ src/main/java/de/ait/javalessons/SpringBootGr61EApplication.java
@@ -0,0 +1,13 @@
+package de.ait.javalessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGr61EApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGr61EApplication.class, args);
+    }
+
+}
